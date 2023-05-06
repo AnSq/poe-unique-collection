09e9e2ba325823db7f23d1cb5ed214cafe4791a1
AnSq <ansq00@gmail.com>
2023-05-06 14:08:34 -0700

    corrupted changes

--- corrupted_export.py
+++ corrupted_export.py
@@ -11,10 +11,9 @@ def main(fname:str=CORRUPTED_EXPORT_FNAME) -> None:
     query = {
         "tables": "mods, mod_spawn_weights",
         "join on": "mods._pageID=mod_spawn_weights._pageID",
-        "fields": 'mods.domain, mods.id, mods.stat_text_raw=text, mods.required_level, GROUP_CONCAT(CONCAT(mod_spawn_weights.tag, ":", mod_spawn_weights.value))=weights, GROUP_CONCAT(mod_spawn_weights.ordinal)=ord,',
+        "fields": 'mods.stat_text_raw=text, GROUP_CONCAT(CONCAT(mod_spawn_weights.tag, ":", mod_spawn_weights.value))=weights',
         "where": "(mods.domain=1 OR mods.domain=10) AND mods.generation_type=5",
-        "group by": "mods.id, mods.domain",
-        "order by": '`cargo__mods`.`id`',
+        "group by": "text",
         "limit": "5000",
         "format": "json",
     }
@@ -23,14 +22,16 @@ def main(fname:str=CORRUPTED_EXPORT_FNAME) -> None:
     data = requests.get(url).json()
 
     for mod in data:
-        if mod["id"] == "V2ChillEffectOnYouJewelCorrupted" and "weights" not in mod:  # this mod is broken in the database
+        if mod["text"] == "(25-20)% reduced Effect of Chill on you" and "weights" not in mod:  # this mod is broken in the database
             mod["weights"] = {"jewel" : 1000}
             continue
 
-        weights = {}
-        for w in mod["weights"].split(","):
-            s = w.split(":")
-            weights[s[0]] = int(s[1])
+        weights:dict[str,int] = {}
+        for weight_spec in mod["weights"].split(","):
+            type_name, value = weight_spec.split(":")
+            value = int(value)
+            if type_name not in weights or value > weights[type_name]:
+                weights[type_name] = value
         mod["weights"] = weights
 
     with open(fname, "w") as f:
